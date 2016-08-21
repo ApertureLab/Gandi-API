0555286ad86bcb2abf93fd3991e489ce1f91a628
Arnaud Ligny <arnaud@ligny.org>
2016-08-21 22:48:19 +0200

    Applied fixes from StyleCI (#2)

--- example.php
+++ example.php
@@ -1,7 +1,8 @@
 <?php
+
 // Composer autoloading
-if (file_exists(__DIR__ . '/vendor/autoload.php')) {
-    $loader = include __DIR__ . '/vendor/autoload.php';
+if (file_exists(__DIR__.'/vendor/autoload.php')) {
+    $loader = include __DIR__.'/vendor/autoload.php';
 }
 
 use ZendService\Gandi\Gandi;
@@ -12,24 +13,24 @@ try {
     $apiKey = 'xxxxxxxxxx';
 
     // account info
-    print_r($gandi->account->info(array($apiKey)));
+    print_r($gandi->account->info([$apiKey]));
 
     // PASS vhost list
-    print_r($gandi->paas->vhost->list(array($apiKey)));
+    print_r($gandi->paas->vhost->list([$apiKey]));
 
     // PASS vhost info
-    print_r($gandi->paas->vhost->info(array($apiKey, 'github.narno.org')));
+    print_r($gandi->paas->vhost->info([$apiKey, 'github.narno.org']));
 
     // PASS create (generic) vhost
-    print_r($gandi->paas->vhost->create(array(
+    print_r($gandi->paas->vhost->create([
         $apiKey,
-        array(
+        [
             'paas_id'    => 00000,
-            'vhost'      => time() . '.narno.org',
+            'vhost'      => time().'.narno.org',
             'override'   => false,
             'zone_alter' => true,
-        )
-    )));
+        ],
+    ]));
 } catch (Exception $e) {
     echo $e->getMessage();
 }
